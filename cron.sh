a0263418a10044cfdbfa5ade1329e345fae78847
Laurent Xu <7594962+laurent-xu@users.noreply.github.com>
2022-08-18 09:32:43 +0000

    Run cron as current user and log result

--- cron.sh
+++ cron.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/bash
 
-ansible-pull -o -vvv -U https://github.com/laurent-xu/notmyfoo-provision.git
+function run_ansible() {
+    ansible_dir=~/.ansible/pull/*
+    ansible-pull -o -vvv -U https://github.com/laurent-xu/notmyfoo-provision.git || rm -rf $ansible_dir
+}
+
+run_ansible | ts '[%Y-%m-%d %H:%M:%S]' >> ~/log/ansible-cron 2>&1
